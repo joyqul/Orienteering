f101a920e2cccdde2d51293f117b961f3de489a8
joyqul <joyqul.tw@gmail.com>
2015-06-03 11:30:47 +0800

    finish server part

--- Server/server.py
+++ Server/server.py
@@ -3,7 +3,7 @@ PACKET_SIZE = 1024
 
 class Server:
     def __init__(self, ip='127.0.0.1', port=10001):
-        port = 1024+random.randint(1, 1000)
+#port = 1024+random.randint(1, 1000)
         self.address = (ip, port)
         self.client = {}
         self.games = []
